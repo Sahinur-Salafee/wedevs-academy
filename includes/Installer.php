57e5de6741db0f9c71f7576f590f75620c7d2776
salafee-xs <72187069+salafee-xs@users.noreply.github.com>
2022-09-26 07:43:33 +0600

    Initial commit

--- includes/Installer.php
+++ includes/Installer.php
@@ -20,7 +20,7 @@ class Installer {
 
     public function add_version()
     {
-        $installed = get_option('wp_academy_installed');
+        $installed       = get_option('wp_academy_installed');
 
         if( ! $installed) {
             update_option('wp_academy_installed', time());
@@ -29,8 +29,29 @@ class Installer {
         update_option('wp_academy_version', WD_ACADEMY_VERSION);
     }
 
+    /**
+     * Create the database tables.
+     */
+
     public function create_tables()
     {
-        
+        global $wpdb;
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $schema          = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ac_addresses` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(100) NOT NULL,
+            `address` varchar(255) DEFAULT NULL,
+            `phone` varchar(30) DEFAULT NULL,
+            `created_by` bigint(20) NOT NULL,
+            `created_at` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+        ) $charset_collate";
+
+        if( ! function_exists('dbDelta')) {
+            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        }
+
+        dbDelta($schema);
     }
 }
