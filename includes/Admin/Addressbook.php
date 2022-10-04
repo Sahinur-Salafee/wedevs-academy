4a28f04f3f0893b58932a39c4c974d2c9aef57bd
salafee-xs <72187069+salafee-xs@users.noreply.github.com>
2022-10-04 07:00:53 +0600

    Data insert method and validation data fields.

--- includes/Admin/Addressbook.php
+++ includes/Admin/Addressbook.php
@@ -8,6 +8,8 @@ namespace Wedevs\Academy\Admin;
 
 class Addressbook {
 
+    public $errors = [];
+
     public function Addressbook()
     {
         $action = isset($_GET['action']) ? $_GET['action'] : 'list';
@@ -54,6 +56,37 @@ class Addressbook {
         if( ! current_user_can('manage_options')) {
             wp_die('Are you cheating?');
         }
+
+        $name    = isset($_POST['name']) ? sanitize_text_field($_POST['name'])          : '';
+        $address = isset($_POST['address']) ? sanitize_textarea_field($_POST['address']): '';
+        $phone   = isset($_POST['phone']) ? sanitize_text_field($_POST['phone'])        : '';
+
+        $insert_id = wd_ac_insert_address([
+            'name'    => $name,
+            'address' => $address,
+            'phone'   => $phone
+        ]);
+
+        if( empty($name)) {
+            $this->errors['name'] = __('Please Provide a name', 'wedevs-academy');
+        }
+
+        if( empty($phone)) {
+            $this->errors['phone'] = __('Please Provide a phone number', 'wedevs-academy');
+        }
+
+        if(!empty($this-> errors)) {
+            return;
+        }
+
+        // If data not inserted
+        if(is_wp_error($insert_id)) {
+            wp_die($insert_id-> get_error_message());
+        }
+
+        // Redirect into admin page
+        $redirect_into = admin_url('admin.php?page=wedevs-academy&inserted=true');
+        wp_redirect($redirect_into);
 
         var_dump($_POST);
         exit;
